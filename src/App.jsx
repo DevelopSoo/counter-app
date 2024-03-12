0b1b7b8b3356626aef5316208e2b3b1012854062
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2024-03-12 14:49:46 +0900

    update: 버튼 컴포넌트 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,24 @@ export default function App() {
 
   return (
     <>
-      <button
-        onClick={minusOne}
-        className="border border-slate-300 rounded-md py-2 px-4"
-      >
-        -
-      </button>
+      {/* onClick(2) => Button 컴포넌트가 받으려고 하는 데이터의 이름 */}
+      {/* minusOne => 실제 기능으로 최종적으로 button 태그 내로 전달하려고 하는 기능 */}
+      <Button onClick={minusOne}>-</Button>
       <span>{count}</span>
-      <button
-        onClick={plusOne}
-        className="border border-slate-300 rounded-md py-2 px-4"
-      >
-        +
-      </button>
+      <Button onClick={plusOne}>+</Button>
     </>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button
+      // button 태그의 onClick(1) => 원래 버튼 태그가 가지고 있는 onClick 속성
+      // {} 내에 있는 onClick(2)은 Button 컴포넌트가 받은 onClick(2)
+      onClick={onClick}
+      className="border border-slate-300 rounded-md py-2 px-4"
+    >
+      {children}
+    </button>
   );
 }
