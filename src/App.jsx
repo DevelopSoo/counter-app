2a4de6bbc694a41bb7ab2198c23b9c1d1f4fff68
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2024-03-12 21:13:56 +0900

    reset 버튼 기능 생성

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,16 @@ export default function App() {
     setCount((prev) => prev + 1);
   };
 
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <>
-      {/* onClick(2) => Button 컴포넌트가 받으려고 하는 데이터의 이름 */}
-      {/* minusOne => 실제 기능으로 최종적으로 button 태그 내로 전달하려고 하는 기능 */}
       <Button onClick={minusOne}>-</Button>
       <span>{count}</span>
       <Button onClick={plusOne}>+</Button>
+      <Button onClick={reset}>reset</Button>
     </>
   );
 }
@@ -25,8 +28,6 @@ export default function App() {
 function Button({ children, onClick }) {
   return (
     <button
-      // button 태그의 onClick(1) => 원래 버튼 태그가 가지고 있는 onClick 속성
-      // {} 내에 있는 onClick(2)은 Button 컴포넌트가 받은 onClick(2)
       onClick={onClick}
       className="border border-slate-300 rounded-md py-2 px-4"
     >
