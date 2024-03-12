b2af32ef9e3771d8a65484335f02d9a61196c93f
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2024-03-12 14:33:04 +0900

    버튼 증감 기능 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">초기 코드</h1>;
+  const [count, setCount] = useState(0);
+
+  const minusOne = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const plusOne = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <>
+      <button
+        onClick={minusOne}
+        className="border border-slate-300 rounded-md py-2 px-4"
+      >
+        -
+      </button>
+      <span>{count}</span>
+      <button
+        onClick={plusOne}
+        className="border border-slate-300 rounded-md py-2 px-4"
+      >
+        +
+      </button>
+    </>
+  );
 }
