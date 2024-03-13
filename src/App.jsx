1e99cae23fa80eb7246d4f14ce1a1a8e17eff0d6
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2024-03-13 09:46:02 +0900

    update: 모달 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   const minusOne = () => {
     setCount((prev) => prev - 1);
@@ -15,13 +16,44 @@ export default function App() {
     setCount(0);
   };
 
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const preventEventBubbling = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <>
-      <Button onClick={minusOne}>-</Button>
-      <span>{count}</span>
-      <Button onClick={plusOne}>+</Button>
-      <Button onClick={reset}>reset</Button>
-    </>
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <div className="border border-slate-300 rounded-md p-6 flex items-center gap-4">
+        <Button onClick={minusOne}>-</Button>
+        <span>{count}</span>
+        <Button onClick={plusOne}>+</Button>
+        <Button onClick={reset}>reset</Button>
+        <Button onClick={openModal}>
+          <SettingIcon />
+        </Button>
+      </div>
+      {/* 모달창 */}
+      {isOpen && (
+        <div
+          onClick={closeModal}
+          className="w-dvw h-dvh fixed bg-black bg-opacity-50 flex justify-center items-center"
+        >
+          <div
+            onClick={preventEventBubbling}
+            className="bg-white rounded-md p-6"
+          >
+            <h1 className="text-xl font-semibold">Settings</h1>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
@@ -33,5 +65,26 @@ function Button({ children, onClick }) {
     >
       {children}
     </button>
+  );
+}
+
+function SettingIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="h-6 w-6"
+      data-id="9"
+    >
+      <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path>
+      <circle cx="12" cy="12" r="3"></circle>
+    </svg>
   );
 }
